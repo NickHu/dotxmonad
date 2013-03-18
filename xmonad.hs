b9deb075dbbc402ed056628f79a1aff566727ddb
Nick Hu <nickhu00@gmail.com>
2013-03-18 19:12:59 +0000

    Style/indentation cleanup

--- xmonad.hs
+++ xmonad.hs
@@ -34,14 +34,14 @@ layouts = hintedTile Tall ||| hintedTile Wide ||| Full
 
 customXPConfig = defaultXPConfig                                    
   {
-      font  = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u" 
-    , fgColor = solarizedBlue
-    , bgColor = solarizedBase02
-    , fgHLight    = solarizedOrange
-    , bgHLight    = solarizedBase02
-    , promptBorderWidth = 0
-    , position = Top
-    , height   = 16
+    font  = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u" 
+  , fgColor = solarizedBlue
+  , bgColor = solarizedBase02
+  , fgHLight    = solarizedOrange
+  , bgHLight    = solarizedBase02
+  , promptBorderWidth = 0
+  , position = Top
+  , height   = 16
   }
 
 manager = composeAll
@@ -65,9 +65,10 @@ main = do
   , ppTitle = taffybarColor solarizedBlue ""
   , ppLayout = taffybarColor solarizedViolet ""
   , ppSep = " <span foreground='" ++ solarizedCyan ++ "'>|</span> "
-    }
+  }
   xmonad . ewmh $ withUrgencyHookC (BorderUrgencyHook solarizedOrange) urgencyConfig { suppressWhen = Focused, remindWhen = Dont } $ defaultConfig
-    { terminal        = "urxvtc"
+    {
+      terminal        = "urxvtc"
     , modMask         = mod4Mask
     , workspaces      = ["sys", "www"]
     , borderWidth     = 1
@@ -84,27 +85,30 @@ main = do
   where
     keyBindings (XConfig {modMask = modm}) = M.fromList $
       [ 
-        -- Workspaces
-          ((modm .|. shiftMask, xK_BackSpace), removeWorkspace)
-        , ((modm .|. shiftMask, xK_v      ), selectWorkspace customXPConfig)
-        , ((modm, xK_i                    ), withWorkspace customXPConfig (windows . W.shift))
-        , ((modm .|. shiftMask, xK_i      ), withWorkspace customXPConfig (windows . copy))
-        , ((modm, xK_o                    ), addWorkspacePrompt customXPConfig)
-        , ((modm .|. shiftMask, xK_r      ), renameWorkspace customXPConfig)
+      -- Workspaces
+        ((modm .|. shiftMask, xK_BackSpace), removeWorkspace)
+      , ((modm .|. shiftMask, xK_v      ), selectWorkspace customXPConfig)
+      , ((modm, xK_i                    ), withWorkspace customXPConfig (windows . W.shift))
+      , ((modm .|. shiftMask, xK_i      ), withWorkspace customXPConfig (windows . copy))
+      , ((modm, xK_o                    ), addWorkspacePrompt customXPConfig)
+      , ((modm .|. shiftMask, xK_r      ), renameWorkspace customXPConfig)
 
-        -- MPC
-        , ((mod1Mask          , xK_comma ), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-        , ((mod1Mask          , xK_period), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-        , ((mod1Mask          , xK_p     ), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-        , ((mod1Mask          , xK_r     ), spawn "mpc random")
+      -- MPC
+      , ((mod1Mask          , xK_comma ), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
+      , ((mod1Mask          , xK_period), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
+      , ((mod1Mask          , xK_p     ), spawn "dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.mpd /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
+      , ((mod1Mask          , xK_r     ), spawn "mpc random")
       ]
-         -- mod-[1..9]       %! Switch to workspace N
-         -- mod-shift-[1..9] %! Move client to workspace N
-          ++
-          zip (zip (repeat (modm)) [xK_1..xK_9]) (map (removeEmptyWorkspaceAfter . withNthWorkspace W.greedyView) [0..])
-          ++
-          zip (zip (repeat (modm .|. shiftMask)) [xK_1..xK_9]) (map (withNthWorkspace W.shift) [0..])
-          ++
-          [((modm .|. mask, key), f sc)
-            | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-            , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]]
+ 
+      -- mod-[1..9]       %! Switch to workspace N
+      -- mod-shift-[1..9] %! Move client to workspace N
+      ++
+      zip (zip (repeat (modm)) [xK_1..xK_9]) (map (removeEmptyWorkspaceAfter . withNthWorkspace W.greedyView) [0..])
+      ++
+      zip (zip (repeat (modm .|. shiftMask)) [xK_1..xK_9]) (map (withNthWorkspace W.shift) [0..])
+
+      -- fix buggy screen order
+      ++
+      [((modm .|. mask, key), f sc)
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]]
